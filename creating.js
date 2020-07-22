72724fccd9ce3e51f7ad59b8197a3b7cff3565e8
vishwanathvishu95 <68235923+vishwanathvishu95@users.noreply.github.com>
2020-07-22 10:15:12 +0530

    Assignment2

---
+++ creating.js
@@ -0,0 +1,68 @@
+class User {
+    constructor(name, age,email) {
+      this.name = name;
+      this.age = age;
+      this.email = email;
+      this.luCoins = 0;
+      this.courses = [];
+    }
+
+    static greet(){
+        console.log("Hello There");
+    }
+
+    login(){
+        console.log(`${this.name} has logged in`);
+        return this;
+    }
+    logout(){
+        console.log(`${this.name} has logged out`);
+        return this;
+    }
+    addCoins(){
+        this.luCoins++;
+        console.log(`${this.name} has ${this.luCoins} coins`);
+        return this;
+    }
+    getDetails(){
+        console.log(`Name is ${this.name}, email is ${this.email}`);
+        return this;
+    }
+
+}
+
+class Moderator extends User{
+    constructor(name,age,email,role){
+        super(name,age,email);
+        this.role = role;
+    }
+
+    deleteUser(user){
+        users = users.filter(u =>{
+            return u.email != user.email; 
+        })
+    }
+
+}
+
+class Admin extends Moderator{
+   addCourse(user,course){
+       user.courses.push(course);
+       console.log(user);
+   }
+}
+
+
+let user1 = new User('sujitha',25,'suji@gmail.com')
+let user2 = new User('sarala',24,'saru@gmai.com')
+let mod = new Moderator('surekha',24,'suru@gmail.com','Moderator');
+let admin = new Admin('sravs',25,'sravs@gmail.com');
+let users = [user1,user2,mod,admin];
+
+users.forEach(element => {
+    console.log(element);
+});
+
+User.greet();
+admin.addCourse(user1,'Javascript');
+admin.addCourse(user1,'Python');
